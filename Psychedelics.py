96bae62cfb77e8a004da0f8172770e82cc616dbf
lamcnguyen89 <63422585+lamcnguyen89@users.noreply.github.com>
2020-06-10 02:59:04 -0400

    Add files via upload

---
+++ Psychedelics.py
@@ -0,0 +1,92 @@
+
+
+shrooms = {
+
+"Psilocybe cubensis",
+"Psilocybe semilanceata", 
+"Psilocybe baeocystis",
+"Psilocybe mexicana",
+"Psilocybe cyanescens",
+"Psilocybe azurescens"
+"Psilocybe semperviva"
+"Psilocybe caerulescens"
+"Psilocybe yungensis"
+"Psilocybe aztecorum"
+"Psilocybe zapotecorum"
+"Psilocybe sphinctrinus"
+"Psilocybe mixaeensis"
+"Panaeolus sphinctrinus"
+"Stropharia cubensis"
+"Conocybe siliginoides"
+"Psilocybe hoogshagenii"
+"Psilocybe wassonii"
+
+
+}
+
+cacti = {
+
+"PEYOTE (lophophora williamsii) ",
+"peyotillo- PELECYPHORA ASELLIFORMIS",
+"tsuwiri-ARIOCARPUS RETUSUS  ",
+"sunami- ARIOCARPUS FISSURATUS",
+"DOÑANA- CORYPHANTHA MACROMERIS",
+"DOLICHOTHELE",
+"San Pedro- Trichocereus pachanoi"
+"Pachycereus pecten-aboriginum"
+
+}
+
+"Amanita muscaria- fly agaric"
+"AGARA (Galbulimima Belgraveana"
+"ERERIBA"
+"KWASHI (Pancratium trianthum)"
+"GALANGA or MARABA (Kaempferia galanga)"
+"MARIHUANA, HASHEESH, or HEMP (species of the genus Cannabis)"
+"TURKESTAN MINT (Lagochilus inebrians)"
+"SYRIAN RUE (Peganum harmala)"
+"KANNA (Mesembryanthemum expansum and M. tortuosum)"
+"BELLADONNA (Atropa belladonna)"
+"HENBANE (Hyoscyamus niger)"
+"MANDRAKE (Mandragora officinarum)"
+"DHATURA and DUTRA (Datura metel)"
+"IBOGA (Tabernanthe iboga)"
+"PUFFBALLS (Lycoperdon mixtecorum and L. marginotum)"
+"RAPE DOS INDIOS (Maquira sclerophylla)"
+"SWEET FLAG (Acorus calamus)"
+"VIROLAS (Virola calophylla, V. colophylloidea, and V. theiodora)"
+"MASHA-HARI (Justicia pectoralis var. stenophylla)"
+"JUREMA (Mimosa hostilis)"
+"YOPO or PARICA (Anadenanthera peregrina or Piptadenia peregrina)"
+"VILCA (Anadenanthera colubrina) and SEBIL"
+"GENISTA (Cytisus canariensis)"
+"MESCAL BEAN (Saphora secundiflora)"
+"COLORINES (several species of Erythrina)"
+"PIULE (several species of Rhynchosia)"
+"Banisteriopsis caapi or B. inebrians"
+"Tetrapteris methistica"
+"SHANSHI (Coriaria thymifolia)"
+"SINICUICHI (Heimia salicifolia)"
+"HIERBA LOCA and TAGLLI (Pernettya furens and P. parvifolia)"
+"SACRED MEXICAN MORNING GLORIES of two species (Rivea corymbosa and Ipomoea violacea)"
+"HOJAS DE LA PASTORA (Salvia divinorum)"
+"COLEUS (Coleus pumas and C. blumei)"
+"BORRACHERA (lochroma fuchsioides)"
+"ARBOL DE LOS BRUJOS ("sorcerers tree") or latué (Latua pubiflora)"
+"CHIRIC-CASPI and CHIRIC SANANGO (Brunfelsia)"
+"DATURAS (Datura)"
+"JIMSON WEED or thorn apple (Datura stramonium)"
+"TORNA-LOCO (Datura ceratocaula)"
+"TOLOACHE (Datura inoxia; known also as D. meteloides)"
+"TREE DATURAS (D. suaveolens, D. arborea, D. aurea, D. candida, D.
+dolichocarpa, D. sanguinea, and the newly discovered D. vulcanicola)"
+"CUIEBRA BORRACHERO (Methysticodendron amesianum)"
+"SHANIN (Petunia violacea)"
+"KEULE (Gomortega keule)"
+"TAIQUE (Desfontainia hookeri)"
+"TUPA (tobelia tupa)"
+"ZACATECHICHI (Calea zacatechichi)"
+
+	  
+python3 bing_scraper.py --search 'ZACATECHICHI plant' --limit 200 --download --chromedriver /home/lam/ml/google-images-download/chromedriver
+ 
